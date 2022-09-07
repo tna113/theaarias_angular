c578e0c02cf6ad11ff3688361198eedb7c8db0a9
ta7384 <ta7384@g.rit.edu>
2022-09-07 12:25:21 -0400

    proj-card component

---
+++ theaarias_angular/src/app/_components/proj-card/proj-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjCardComponent } from './proj-card.component';
+
+describe('ProjCardComponent', () => {
+  let component: ProjCardComponent;
+  let fixture: ComponentFixture<ProjCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
