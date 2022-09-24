b4999977046d9f34b050e5ffe2c49cbe81f29971
ta7384 <ta7384@g.rit.edu>
2022-09-24 14:13:59 -0400

    project card styling, project card container component created, added project data

--- theaarias_angular/src/app/app.module.ts
+++ theaarias_angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjCardComponent } from './_components/proj-card/proj-card.component';
+import { CardContainerComponent } from './_components/card-container/card-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjCardComponent
+    ProjCardComponent,
+    CardContainerComponent
   ],
   imports: [
     BrowserModule,
